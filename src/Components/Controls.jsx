31185acb5fd7b538af03d7b83fdd7da308457a72
Robert Barta <ro.barta@icloud.com>
2020-08-30 16:12:11 +0200

    Adds FontAwesome

--- src/Components/Controls.jsx
+++ src/Components/Controls.jsx
@@ -1,115 +1,122 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 class Controls extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      numPoints: 100,
-      enabled: [true, true, true, true, true, true, true, true, true],
-      sliderEnabled: true,
+        this.state = {
+            numPoints: 100,
+            enabled: [true, true, true, true, true, true, true, true, true],
+            sliderEnabled: true,
+        };
+    }
+
+    childFunction = (algo) => {
+        this.props.functionCallFromParent(algo);
+
+        const algoButtons = document.getElementsByClassName("algoBtn");
+        var enabled = [];
+        const numBars = this.state.numPoints;
+
+        var sliderEnabled = false;
+
+        if (algo !== 0) {
+            if (!this.state.enabled[0]) {
+                for (let i = 0; i < this.state.enabled.length; ++i) {
+                    enabled[i] = true;
+                    algoButtons[algo - 1].id = "none";
+                }
+                sliderEnabled = true;
+            } else {
+                for (let i = 0; i < this.state.enabled.length; ++i) {
+                    enabled[i] = false;
+                }
+
+                enabled[algo] = true;
+                algoButtons[algo - 1].id = "playing";
+            }
+
+            this.setState({ numBars, enabled, sliderEnabled });
+        }
     };
-  }
 
-  childFunction = (algo) => {
-    this.props.functionCallFromParent(algo);
+    enableUI() {
+        const buttons = document.getElementsByClassName("algoBtn");
 
-    const algoButtons = document.getElementsByClassName("algoBtn");
-    var enabled = [];
-    const numBars = this.state.numPoints;
+        const enabled = [true];
 
-    var sliderEnabled = false;
-
-    if (algo !== 0) {
-      if (!this.state.enabled[0]) {
-        for (let i = 0; i < this.state.enabled.length; ++i) {
-          enabled[i] = true;
-          algoButtons[algo - 1].id = "none";
-        }
-        sliderEnabled = true;
-      } else {
-        for (let i = 0; i < this.state.enabled.length; ++i) {
-          enabled[i] = false;
+        for (let i = 0; i < buttons.length; ++i) {
+            enabled.push(true);
+            buttons[i].id = "none";
         }
 
-        enabled[algo] = true;
-        algoButtons[algo - 1].id = "playing";
-      }
+        const numBars = this.state.numPoints;
 
-      this.setState({ numBars, enabled, sliderEnabled });
-    }
-  };
-
-  enableUI() {
-    const buttons = document.getElementsByClassName("algoBtn");
-
-    const enabled = [true];
-
-    for (let i = 0; i < buttons.length; ++i) {
-      enabled.push(true);
-      buttons[i].id = "none";
+        this.setState({ numBars, enabled, sliderEnabled: true });
     }
 
-    const numBars = this.state.numPoints;
+    updateBars = () => {
+        this.props.updateBars();
+    };
 
-    this.setState({ numBars, enabled, sliderEnabled: true });
-  }
+    setNumBars() {
+        var slider = document.getElementById("numberOfBars");
+        const numBars = slider.value;
+        //this.resetArray();
+        this.setState({ numBars });
+        this.updateBars();
+    }
 
-  updateBars = () => {
-    this.props.updateBars();
-  };
-
-  setNumBars() {
-    var slider = document.getElementById("numberOfBars");
-    const numBars = slider.value;
-    //this.resetArray();
-    this.setState({ numBars });
-    this.updateBars();
-  }
-
-  render() {
-    return (
-      <div className="controls">
-        <button
-          onClick={() => this.childFunction(0)}
-          className="randomizeBtn"
-          disabled={!this.state.enabled[0]}
-        >
-          Randomize
-        </button>
-        <button
-          onClick={() => this.childFunction(1)}
-          className="algoBtn"
-          id="jarvisBtn"
-          disabled={!this.state.enabled[1]}
-        >
-          Jarvis March
-        </button>
-        <button
-          onClick={() => this.childFunction(2)}
-          className="algoBtn"
-          id="grahamBtn"
-          disabled={!this.state.enabled[2]}
-        >
-          Graham Scan
-        </button>
-        <button
-          onClick={() => this.childFunction(3)}
-          className="algoBtn"
-          id="quickhullBtn"
-          disabled={!this.state.enabled[3]}
-        >
-          QuickHull
-        </button>
-        <button
-          onClick={() => this.childFunction(7)}
-          className="deleteBtn"
-          disabled={!this.state.enabled[7]}
-        >
-          Delete
-        </button>
-        {/* <br />
+    render() {
+        return (
+            <div className="controls">
+                <button
+                    onClick={() => this.childFunction(0)}
+                    className="randomizeBtn"
+                    disabled={!this.state.enabled[0]}
+                >
+                    Randomize
+                </button>
+                <button
+                    onClick={() => this.childFunction(1)}
+                    className="algoBtn"
+                    id="jarvisBtn"
+                    disabled={!this.state.enabled[1]}
+                >
+                    Jarvis March
+                </button>
+                <button
+                    onClick={() => this.childFunction(2)}
+                    className="algoBtn"
+                    id="grahamBtn"
+                    disabled={!this.state.enabled[2]}
+                >
+                    Graham Scan
+                </button>
+                <button
+                    onClick={() => this.childFunction(3)}
+                    className="algoBtn"
+                    id="quickhullBtn"
+                    disabled={!this.state.enabled[3]}
+                >
+                    QuickHull
+                </button>
+                <button
+                    onClick={() => this.childFunction(7)}
+                    className="deleteBtn"
+                    disabled={!this.state.enabled[7]}
+                >
+                    <FontAwesomeIcon
+                        className="trash"
+                        transform="shrink-3 up-3"
+                        size="2x"
+                        icon={faTrash}
+                    />
+                </button>
+                {/* <br />
         <p>Number of bars: {this.state.numPoints}</p>
         <input
           type="range"
@@ -121,9 +128,9 @@ class Controls extends Component {
           id="numberOfPoints"
           onChange={() => this.setNumBars()}
         /> */}
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Controls;
